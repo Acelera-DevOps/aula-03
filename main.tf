4127dfe4fc194d6a05599c5e563ee2522385da6c
Erik Tonon <eriktonon@gmail.com>
2024-02-16 20:26:28 -0300

    aula 03 update

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 module "my_vpc" {
-  source = "./modules/aws-vpc"
+  source     = "./modules/aws-vpc"
   cidr_block = "10.0.0.0/16"
 }
--- modules/aws-vpc/outputs.tf
+++ modules/aws-vpc/outputs.tf
@@ -2,5 +2,20 @@
 
 output "vpc_id" {
   value = aws_vpc.example.id
-  description = "The ID of the VPC"
+  description = "esse é o id da vpc"
+}
+
+output "aws_subnet" {
+  value = aws_subnet.example-subnet.id
+  descripiton =  "esse é o id da subnet"
+}
+
+output "aws_internet_gateway" {
+  value = aws_internet_gateway.example.id
+  description = "esse é o id do gateway"
+}
+
+output "aws_route_table" {
+  value = aws_route_table.rt.id
+  description = "esse é o id da route table"
 }
